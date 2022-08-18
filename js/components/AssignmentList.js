2446a468409bd58301294efce4ca226223433af4
Diego Melgar <diego.melgar.sv@gmail.com>
2022-08-18 10:07:02 -0600

    ✨ Handle form submition

--- js/components/AssignmentList.js
+++ js/components/AssignmentList.js
@@ -4,9 +4,9 @@ export default {
   components: { Assignment },
   template: `
     <section v-if="assignments.length">
-        <h2 class="font-bold mb-2">{{ title }}</h2>
+        <h2 class="font-bold mb-2"> {{ title }} </h2>
 
-        <ul>
+        <ul class="border border-gray-600 divide-y divide-gray-600">
           <assignment 
             v-for="assignment in assignments" 
             :key="assignment.id"
--- js/components/Assignments.js
+++ js/components/Assignments.js
@@ -6,7 +6,15 @@ export default {
       <section class="space-y-6">
         <assignment-list title="In progress" :assignments="filters.inProgress"></assignment-list>
         <assignment-list title="Completed" :assignments="filters.completed"></assignment-list>
+
+        <form @submit.prevent="add">
+          <div class="border border-gray-600 text-black">
+            <input placeholder="New assignment..." class="p-2" v-model="newAssignment">
+            <button type="submit" class="bg-white p-2 border-l"> Add </button>
+          </div>
+        </form>
       </section>
+
       `,
 
   data() {
@@ -16,8 +24,11 @@ export default {
         { name: "Read chapter 4", complete: false, id: 2 },
         { name: "Turn in homework", complete: false, id: 3 },
       ],
+
+      newAssignment: "",
     };
   },
+
   computed: {
     filters() {
       return {
@@ -26,6 +37,18 @@ export default {
         ),
         completed: this.assignments.filter((assignment) => assignment.complete),
       };
+    },
+  },
+
+  methods: {
+    add() {
+      this.assignments.push({
+        name: this.newAssignment,
+        complete: false,
+        id: this.assignments.length + 1,
+      });
+
+      this.newAssignment = "";
     },
   },
 };
